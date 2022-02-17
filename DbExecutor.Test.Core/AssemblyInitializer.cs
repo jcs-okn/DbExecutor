e9e2439e571ee64b599094623f0884cce0086106
k-oshiro <k-oshiro@jcs-ltd.com>
2022-02-17 11:40:00 +0900

    corresponded to dotnet core.

---
+++ DbExecutor.Test.Core/AssemblyInitializer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DbExecutorTestCore {
+
+    [TestClass]
+    public class AssemblyInitializer {
+
+        [AssemblyInitialize]
+        public static void Init(TestContext tc)
+        {
+
+            DbExecutorTest.AssemblyInitializer.Init(tc);
+
+        }
+    }
+}
